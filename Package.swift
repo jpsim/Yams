c64d4982aac2f9881bde45e90ac49622c1c61d37
JP Simard <jp@jpsim.com>
2017-11-01 17:11:50 -0700

    Move Package@swift-4.swift to Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,15 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Yams",
+    products: [
+      .library(name: "Yams", targets: ["Yams"])
+    ],
     targets: [
-        Target(name: "Yams", dependencies: ["CYaml"])
-    ]
+        .target(name: "CYaml"),
+        .target(name: "Yams", dependencies: ["CYaml"]),
+        .testTarget(name: "YamsTests", dependencies: ["Yams"])
+    ],
+    swiftLanguageVersions: [3, 4]
 )
