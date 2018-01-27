718ed26358fd354891d80870bdc6cfd12630c963
Norio Nomura <norio.nomura@gmail.com>
2018-01-27 22:25:20 +0900

    Change access level of `compactMap()` from `public` to `internal`

--- Sources/Yams/shim.swift
+++ Sources/Yams/shim.swift
@@ -9,19 +9,19 @@
 #if !swift(>=4.1)
 
     extension Array {
-        public func compactMap(_ transform: (Element) throws -> String?) rethrows -> [String] {
+        func compactMap(_ transform: (Element) throws -> String?) rethrows -> [String] {
             return try flatMap(transform)
         }
     }
 
     extension LazyMapCollection {
-        public func compactMap(_ transform: (Element) throws -> String?) rethrows -> [String] {
+        func compactMap(_ transform: (Element) throws -> String?) rethrows -> [String] {
             return try flatMap(transform)
         }
     }
 
     extension Sequence {
-        public func compactMap<ElementOfResult>(
+        func compactMap<ElementOfResult>(
             _ transform: (Self.Element
             ) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
             return try flatMap(transform)
