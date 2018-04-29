d9416f03f88865e32e616369ba04bbc8c246f2c0
JP Simard <jp@jpsim.com>
2018-04-29 16:02:27 -0700

    Adjust indentation in Parser.swift

--- Sources/Yams/Parser.swift
+++ Sources/Yams/Parser.swift
@@ -7,7 +7,7 @@
 //
 
 #if SWIFT_PACKAGE
-    import CYaml
+import CYaml
 #endif
 import Foundation
 
@@ -113,23 +113,23 @@ public final class Parser {
         self.constructor = constructor
 
         yaml_parser_initialize(&parser)
-        #if USE_UTF8
-            yaml_parser_set_encoding(&parser, YAML_UTF8_ENCODING)
-            utf8CString = string.utf8CString
-            utf8CString.withUnsafeBytes { bytes in
-                let input = bytes.baseAddress?.assumingMemoryBound(to: UInt8.self)
-                yaml_parser_set_input_string(&parser, input, bytes.count - 1)
-            }
-        #else
-            // use native endian
-            let isLittleEndian = 1 == 1.littleEndian
-            yaml_parser_set_encoding(&parser, isLittleEndian ? YAML_UTF16LE_ENCODING : YAML_UTF16BE_ENCODING)
-            let encoding: String.Encoding = isLittleEndian ? .utf16LittleEndian : .utf16BigEndian
-            data = yaml.data(using: encoding)!
-            data.withUnsafeBytes { bytes in
-                yaml_parser_set_input_string(&parser, bytes, data.count)
-            }
-        #endif
+#if USE_UTF8
+        yaml_parser_set_encoding(&parser, YAML_UTF8_ENCODING)
+        utf8CString = string.utf8CString
+        utf8CString.withUnsafeBytes { bytes in
+            let input = bytes.baseAddress?.assumingMemoryBound(to: UInt8.self)
+            yaml_parser_set_input_string(&parser, input, bytes.count - 1)
+        }
+#else
+        // use native endian
+        let isLittleEndian = 1 == 1.littleEndian
+        yaml_parser_set_encoding(&parser, isLittleEndian ? YAML_UTF16LE_ENCODING : YAML_UTF16BE_ENCODING)
+        let encoding: String.Encoding = isLittleEndian ? .utf16LittleEndian : .utf16BigEndian
+        data = yaml.data(using: encoding)!
+        data.withUnsafeBytes { bytes in
+            yaml_parser_set_input_string(&parser, bytes, data.count)
+        }
+#endif
         try parse() // Drop YAML_STREAM_START_EVENT
     }
 
