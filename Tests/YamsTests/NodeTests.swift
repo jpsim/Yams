b7e4f406d90d5f890d83ac1922bd74573dd739a3
JP Simard <jp@jpsim.com>
2020-11-04 12:03:50 -0500

    Re-enable failing NodeTests & SpecTests with SSH enabled

--- Tests/YamsTests/NodeTests.swift
+++ Tests/YamsTests/NodeTests.swift
@@ -208,7 +208,21 @@ extension NodeTests {
     static var allTests: [(String, (NodeTests) -> () throws -> Void)] {
 #if os(Windows)
         // TODO: Fix these tests on Windows
-        return []
+        return [
+            ("testExpressibleByArrayLiteral", testExpressibleByArrayLiteral),
+            ("testExpressibleByDictionaryLiteral", testExpressibleByDictionaryLiteral),
+            ("testExpressibleByFloatLiteral", testExpressibleByFloatLiteral),
+            ("testExpressibleByIntegerLiteral", testExpressibleByIntegerLiteral),
+            ("testExpressibleByStringLiteral", testExpressibleByStringLiteral),
+            ("testTypedAccessorProperties", testTypedAccessorProperties),
+            ("testArray", testArray),
+            ("testSubscriptMapping", testSubscriptMapping),
+            ("testSubscriptSequence", testSubscriptSequence),
+            ("testSubscriptWithNonDefaultResolver", testSubscriptWithNonDefaultResolver),
+            ("testMappingBehavesLikeADictionary", testMappingBehavesLikeADictionary),
+            ("testSequenceBehavesLikeAnArray", testSequenceBehavesLikeAnArray),
+            ("testScalar", testScalar)
+        ]
 #else
         return [
             ("testExpressibleByArrayLiteral", testExpressibleByArrayLiteral),
