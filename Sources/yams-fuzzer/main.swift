5b55ed9b18b659be7f54696697347852ec533ef4
JP Simard <jp@jpsim.com>
2021-11-18 12:07:22 -0800

    rename entry point

--- Sources/yams-fuzzer/main.swift
+++ Sources/yams-fuzzer/main.swift
@@ -2,7 +2,7 @@ import Backtrace
 import Foundation
 import Yams
 
-@_cdecl("LLVMFuzzerTestOneInput") public func fuzzMe(data: UnsafePointer<CChar>, size: CInt) -> CInt {
+@_cdecl("LLVMFuzzerTestOneInput") public func fuzzerEntry(data: UnsafePointer<CChar>, size: CInt) -> CInt {
     Backtrace.install()
     let string = String(cString: data)
     _ = try? Yams.load(yaml: string)
