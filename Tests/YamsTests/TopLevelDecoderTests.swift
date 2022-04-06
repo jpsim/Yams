0de508facb046d6b00558ee55f2606a8eee6d0ad
Liam Nichols <liam.nichols.ln@gmail.com>
2022-04-06 13:26:43 +0200

    Add TopLevelDecoderTests.testDecodeOptionalTypes() to reproduce jpsim/Yams#301

--- Tests/YamsTests/TopLevelDecoderTests.swift
+++ Tests/YamsTests/TopLevelDecoderTests.swift
@@ -32,5 +32,51 @@ class TopLevelDecoderTests: XCTestCase {
             )
         XCTAssertEqual(foo?.name, "Bird")
     }
+
+    func testDecodeOptionalTypes() throws {
+        let yaml = """
+        A: ''
+        B:
+        C: null
+        D: ~
+        E: ""
+        json: {
+          "F": "",
+          "G": "null"
+        }
+        array:
+        - one
+        - ''
+        - null
+        - 'null'
+        - '~'
+        """
+
+        struct Container: Codable, Equatable {
+            struct JSON: Codable, Equatable {
+                var F: String?
+                var G: String?
+            }
+
+            var A: String?
+            var B: String?
+            var C: Int?
+            var D: String?
+            var E: String?
+            var json: JSON
+            var array: [String?]
+        }
+
+        let container = try YAMLDecoder().decode(Container.self, from: yaml)
+
+        XCTAssertEqual(container.A, "")
+        XCTAssertEqual(container.B, nil)
+        XCTAssertEqual(container.C, nil)
+        XCTAssertEqual(container.D, nil)
+        XCTAssertEqual(container.E, "")
+        XCTAssertEqual(container.json.F, "")
+        XCTAssertEqual(container.json.G, "null")
+        XCTAssertEqual(container.array, ["one", "", nil, "null", "~"])
+    }
 }
 #endif
