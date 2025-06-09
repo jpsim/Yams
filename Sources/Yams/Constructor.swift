d9342ff1f379b85987f44a8021acc4e694a55299
Tomasz Popis <tomek@melon-it.com>
2025-06-09 13:39:02 +0200

    Convert static let to computed static var for dynamic mapping.

--- Sources/Yams/Constructor.swift
+++ Sources/Yams/Constructor.swift
@@ -102,17 +102,17 @@ extension Constructor {
     ] }
 
     ///`Tag.Name` to `Node.Mapping` map that support dynamic collection.
-    public static let dynamicMappingMap: MappingMap = [
-        .map: NSMutableDictionary.construct_mapping,
-        .set: NSMutableSet.construct_set
-    ]
+    public static var dynamicMappingMap: MappingMap { [
+      .map: NSMutableDictionary.construct_mapping,
+      .set: NSMutableSet.construct_set
+    ] }
 
     ///`Tag.Name` to `Node.Sequence` map that support dynamic collection.
-    public static let dynamicSequenceMap: SequenceMap = [
-        .seq: NSMutableArray.construct_seq,
-        .omap: NSMutableArray.construct_omap,
-        .pairs: NSMutableArray.construct_pairs
-    ]
+    public static var dynamicSequenceMap: SequenceMap { [
+      .seq: NSMutableArray.construct_seq,
+      .omap: NSMutableArray.construct_omap,
+      .pairs: NSMutableArray.construct_pairs
+    ] }
 }
 
 // MARK: - ScalarConstructible
